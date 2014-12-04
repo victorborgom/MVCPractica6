f106dc1895977ccceba8d570e561ac8deb8bba2b
victorborgom <victorborgom@gmail.com>
2014-12-05 00:41:41 +0100

    MVCPractica6
    Practica 6, MVC arrancar core

---
+++ sys/core.php
@@ -0,0 +1,10 @@
+<?php
+    class Core{
+        
+        static function init(){
+            echo "BIENVENIDO, CORE ARRANCADO";
+            //recuperar la request
+        }
+        
+        
+    }
